06c0dfab62f6e67baf82ff57f130d501aea9b61e
Daniel Aprillio <danielaprillio02@gmail.com>
2021-11-30 17:07:01 +0700

    user table

--- Radiance_Website/Radiance/database/migrations/2014_10_12_000000_create_users_table.php
+++ Radiance_Website/Radiance/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id('uid');
+            $table->string('name',255);
+            $table->string('username',255);
+            $table->string('email',255)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('is_login',['0','1']);
+            $table->enum('is_active',['0','1']);
+            $table->string('profile_picture')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
