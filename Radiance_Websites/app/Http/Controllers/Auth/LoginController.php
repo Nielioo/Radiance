8278171baf2a669866b56d47722437ced99ba01b
Bryan <bryanmuliawan@yahoo.co.id>
2021-12-05 04:24:16 +0700

    Add login and logout action
    Change is_login column on fis11_students table based on action

--- Radiance_Websites/app/Http/Controllers/Auth/LoginController.php
+++ Radiance_Websites/app/Http/Controllers/Auth/LoginController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Student;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
-    /*
+	/*
     |--------------------------------------------------------------------------
     | Login Controller
     |--------------------------------------------------------------------------
@@ -19,22 +23,54 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+	use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+	/**
+	 * Where to redirect users after login.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest')->except('logout');
+	}
+
+	/**
+	 * Actions post login.
+	 */
+	protected function authenticated(Request $request, Student $student)
+	{
+		DB::table('fis11_students')
+			->where('student_id', $student->id)
+			->update([
+				'is_login' => '1',
+			]);
+
+		return redirect()->intended($this->redirectPath());
+	}
+
+	/**
+	 * Actions of logout.
+	 */
+	protected function logout()
+	{
+		$student_id = Auth::id();
+
+		Auth::logout();
+
+		DB::table('fis11_students')
+			->where('student_id', $student_id)
+			->update([
+				'is_login' => '0',
+			]);
+
+		return redirect('/');
+	}
 }
