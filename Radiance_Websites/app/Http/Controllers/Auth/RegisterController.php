f82eed207f458d3da1a85889bdadd74bb270c13a
Bryan <bryanmuliawan@yahoo.co.id>
2021-12-05 03:56:32 +0700

    Add registration action
    Insert data to fis11_students table

--- Radiance_Websites/app/Http/Controllers/Auth/RegisterController.php
+++ Radiance_Websites/app/Http/Controllers/Auth/RegisterController.php
@@ -6,13 +6,16 @@ use App\Http\Controllers\Controller;
 use App\Models\Student;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    /*
+	/*
     |--------------------------------------------------------------------------
     | Register Controller
     |--------------------------------------------------------------------------
@@ -23,52 +26,78 @@ class RegisterController extends Controller
     |
     */
 
-    use RegistersUsers;
+	use RegistersUsers;
 
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+	/**
+	 * Where to redirect users after registration.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest');
+	}
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:students'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-    }
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array  $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function validator(array $data)
+	{
+		return Validator::make($data, [
+			'username' => ['required', 'string', 'max:255', 'unique:students'],
+			'name' => ['required', 'string', 'max:255'],
+			'email' => ['required', 'string', 'email', 'max:255', 'unique:students'],
+			'password' => ['required', 'string', 'min:8', 'confirmed'],
+		]);
+	}
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\Models\Student
-     */
-    protected function create(array $data)
-    {
-        return Student::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
-    }
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array  $data
+	 * @return \App\Models\Student
+	 */
+	protected function create(array $data)
+	{
+		return Student::create([
+			'username' => $data['username'],
+			'name' => $data['name'],
+			'email' => $data['email'],
+			'password' => Hash::make($data['password']),
+		]);
+	}
+
+	/**
+	 * Actions of registration.
+	 */
+	protected function register(Request $request)
+	{
+		$this->validator($request->all())->validate();
+
+		event(new Registered($student = $this->create($request->all())));
+
+		// Start of custom action
+		DB::table('fis11_students')->insert([
+			'student_id' => $student->id,
+			'is_login' => '1',
+			'created_at' => now(),
+			'updated_at' => now(),
+		]);
+		// End of custom action
+
+		$this->guard()->login($student);
+
+		return $this->registered($request, $student)
+			?: redirect($this->redirectPath());
+	}
 }
