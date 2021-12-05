533d2ca6bd85232844af747b82a8e2af577d345b
Daniel Aprillio <danielaprillio02@gmail.com>
2021-12-05 15:44:35 +0700

    main page animation done

--- Radiance_Websites/resources/views/layout/mainLayout.blade.php
+++ Radiance_Websites/resources/views/layout/mainLayout.blade.php
@@ -5,21 +5,102 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> @yield('title') </title>
-    <link rel="stylesheet" href="/css/@yield('css').css">
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css"
-        integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/css/mainLayout.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/03f2b35007.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    @yield('content')
+    <section>
+        @for ($i = 1; $i <= 50; $i++)
+            <div class="rows">
+                <div>
+                    <i class="white fas fa-brain"></i>
+                    <i class="white fas fa-dna"></i>
+                    <i class="white fas fa-filter"></i>
+                    <i class="white fas fa-eye-dropper"></i>
+                    <i class="white fas fa-rocket"></i>
+                    <i class="white fas fa-flask"></i>
+                    <i class="white fas fa-seedling"></i>
+                    <i class="white fas fa-band-aid"></i>
+                    <i class="white fas fa-temperature-high"></i>
+                    <i class="white fas fa-microscope"></i>
+                    <i class="white fas fa-book-open"></i>
+                    <i class="white fas fa-brain"></i>
+                    <i class="white fas fa-dna"></i>
+                    <i class="white fas fa-filter"></i>
+                    <i class="white fas fa-eye-dropper"></i>
+                    <i class="white fas fa-rocket"></i>
+                    <i class="white fas fa-flask"></i>
+                    <i class="white fas fa-seedling"></i>
+                    <i class="white fas fa-band-aid"></i>
+                    <i class="white fas fa-temperature-high"></i>
+                    <i class="white fas fa-microscope"></i>
+                    <i class="white fas fa-book-open"></i>
+                    <i class="white fas fa-brain"></i>
+                    <i class="white fas fa-dna"></i>
+                    <i class="white fas fa-filter"></i>
+                    <i class="white fas fa-eye-dropper"></i>
+                    <i class="white fas fa-rocket"></i>
+                    <i class="white fas fa-flask"></i>
+                    <i class="white fas fa-seedling"></i>
+                    <i class="white fas fa-band-aid"></i>
+                    <i class="white fas fa-temperature-high"></i>
+                    <i class="white fas fa-microscope"></i>
+                    <i class="white fas fa-book-open"></i>
+                </div>
+                <div>
+                    <i class="white fas fa-brain"></i>
+                    <i class="white fas fa-dna"></i>
+                    <i class="white fas fa-filter"></i>
+                    <i class="white fas fa-eye-dropper"></i>
+                    <i class="white fas fa-rocket"></i>
+                    <i class="white fas fa-flask"></i>
+                    <i class="white fas fa-seedling"></i>
+                    <i class="white fas fa-band-aid"></i>
+                    <i class="white fas fa-temperature-high"></i>
+                    <i class="white fas fa-microscope"></i>
+                    <i class="white fas fa-book-open"></i>
+                    <i class="white fas fa-brain"></i>
+                    <i class="white fas fa-dna"></i>
+                    <i class="white fas fa-filter"></i>
+                    <i class="white fas fa-eye-dropper"></i>
+                    <i class="white fas fa-rocket"></i>
+                    <i class="white fas fa-flask"></i>
+                    <i class="white fas fa-seedling"></i>
+                    <i class="white fas fa-band-aid"></i>
+                    <i class="white fas fa-temperature-high"></i>
+                    <i class="white fas fa-microscope"></i>
+                    <i class="white fas fa-book-open"></i>
+                    <i class="white fas fa-brain"></i>
+                    <i class="white fas fa-dna"></i>
+                    <i class="white fas fa-filter"></i>
+                    <i class="white fas fa-eye-dropper"></i>
+                    <i class="white fas fa-rocket"></i>
+                    <i class="white fas fa-flask"></i>
+                    <i class="white fas fa-seedling"></i>
+                    <i class="white fas fa-band-aid"></i>
+                    <i class="white fas fa-temperature-high"></i>
+                    <i class="white fas fa-microscope"></i>
+                    <i class="white fas fa-book-open"></i>
+                </div>
+            </div>
+        @endfor
+        <div class="cursor"></div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous">
-    </script>
+        <div class="container-fluid">
+            @yield('mainContent')
+        </div>
+
+        <script>
+            const cursor = document.querySelector('.cursor');
+            document.addEventListener('mousemove', (e) => {
+                cursor.style.left = e.clientX + 'px';
+                cursor.style.top = e.clientY + 'px';
+            })
+        </script>
 </body>
 
 </html>
--- Radiance_Websites/resources/views/main.blade.php
+++ Radiance_Websites/resources/views/main.blade.php
@@ -2,30 +2,42 @@
 
 @section('title', 'MainPage')
 
-@section('css', 'main')
-
-@section('content')
-
-    <div class="bg">
-        <div class="navigation">
-            <h1>Navigation</h1>
-        </div>
-        <div class="context">
-            <p id="radiance">Radiance</p>
-            <div class="cursor"></div>
-        </div>
-
-        <div class="items">
-            <i class="fa fa-brain"></i>
-        </div>
+@section('mainContent')
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sit asperiores laudantium ipsam voluptate ipsa
+        suscipit neque, eligendi nihil autem.</h1><br>
+    <div class="context">
+        <p id="radiance">Radiance</p>
+        <div class="cursor"></div>
     </div>
-
-    <script>
-        const cursor = document.querySelector('.cursor');
-        document.addEventListener('mousemove', (e) => {
-            cursor.style.left = e.clientX + 'px';
-            cursor.style.top = e.clientY + 'px';
-        })
-    </script>
 
 @endsection
