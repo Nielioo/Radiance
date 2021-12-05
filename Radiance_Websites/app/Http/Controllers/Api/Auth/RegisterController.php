cbc298b0ed7122daa3db3c5f5e12d18985e945b7
Bryan <bryanmuliawan@yahoo.co.id>
2021-12-05 22:33:09 +0700

    Create API register

---
+++ Radiance_Websites/app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+	{
+		$this->validate($request, [
+            'username' => 'required|unique:students,username',
+			'name' => 'required',
+			'email' => 'required|unique:students,email',
+			'password' => 'required|string|min:8|confirmed',
+		]);
+
+		$student = $this->newStudent($request->all());
+
+		if(empty($student)) {
+			return response([
+				'message' => 'Failed to create account',
+			]);
+		} else {
+			return response([
+				'message' => 'Account successfully created',
+			]);
+		}
+	}
+
+	private function newStudent($data)
+	{
+        $student = Student::create([
+            'username' => $data['username'],
+			'name' => $data['name'],
+			'email'=> $data['email'],
+			'password' => Hash::make($data['password']),
+		]);
+
+        DB::table('fis11_students')->insert([
+			'student_id' => $student->id,
+			// 'is_login' => '1',
+			'created_at' => now(),
+			'updated_at' => now(),
+		]);
+
+		return $student;
+	}
+}
