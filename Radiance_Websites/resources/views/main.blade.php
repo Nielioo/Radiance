23f3f371e8f8b57851fd6592d90c79e0aeacd316
Daniel Aprillio <danielaprillio02@gmail.com>
2021-12-05 17:26:13 +0700

    main layout done

--- Radiance_Websites/resources/views/main.blade.php
+++ Radiance_Websites/resources/views/main.blade.php
@@ -8,13 +8,12 @@
         <p id="radiance">Radiance</p>
         <div class="cursor"></div>
     </div>
+
     <a id="play" class="play-button" href="#">
         <span></span>
-      </a>
-
-
-      <div id="overlay" class="overlay">
+    </a>
+    <div id="overlay" class="overlay">
         <a class="overlay-close">&times;</a>
-      </div>
+    </div>
 
 @endsection
