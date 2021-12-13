c7749c10cd21e9706c9325c8a90d96e56d38bbfb
Bryan <bryanmuliawan@yahoo.co.id>
2021-12-13 12:14:50 +0700

    Create game problem related database

---
+++ Radiance_Websites/app/Http/Controllers/Fis11GameProblemController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Fis11GameProblem;
+use App\Http\Requests\StoreFis11GameProblemRequest;
+use App\Http\Requests\UpdateFis11GameProblemRequest;
+
+class Fis11GameProblemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreFis11GameProblemRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreFis11GameProblemRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Fis11GameProblem  $fis11GameProblem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Fis11GameProblem $fis11GameProblem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Fis11GameProblem  $fis11GameProblem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Fis11GameProblem $fis11GameProblem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateFis11GameProblemRequest  $request
+     * @param  \App\Models\Fis11GameProblem  $fis11GameProblem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateFis11GameProblemRequest $request, Fis11GameProblem $fis11GameProblem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Fis11GameProblem  $fis11GameProblem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Fis11GameProblem $fis11GameProblem)
+    {
+        //
+    }
+}
