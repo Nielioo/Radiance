ed5369310250d7a99db948f3d1377933937a1177
Daniel Aprillio <danielaprillio02@gmail.com>
2021-12-13 09:01:06 +0700

    Revert "... 2"
    This reverts commit bc26308485ed50fe0510cac1de75cac84f176fc5.

---
+++ Radiance_Mobile_Apps/app/src/main/java/com/radiance/radiance/view/RegisterActivity.java
@@ -0,0 +1,20 @@
+<<<<<<< Updated upstream:Radiance_Mobile_Apps/app/src/main/java/com/radiance/radiance/MainActivity.java
+package com.radiance.radiance;
+||||||| constructed merge base:Radiance_Mobile_Apps/app/src/main/java/com/radiance/radiance/view/RegisterActivity.java
+package com.radiance.radiance.view;
+=======
+package com.radiance.radiance.view.auth;
+>>>>>>> Stashed changes:Radiance_Mobile_Apps/app/src/main/java/com/radiance/radiance/auth/RegisterActivity.java
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
