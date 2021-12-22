743eb5122de557b22a3922900fd837985fa0061f
annaxatma <83283641+annaxatma@users.noreply.github.com>
2021-12-22 23:09:28 +0900

    mood2annya hilanggg YEAYY

--- Radiance_Websites/resources/views/contents/mainMode.blade.php
+++ Radiance_Websites/resources/views/contents/mainMode.blade.php
@@ -9,12 +9,12 @@
     <div class="mode">
         <a class="img-mode" href="/inStory">
             <img class="img-fluid" src="/img/mainMode/unhover/story_mode_dark.png" alt="Story Mode"
-            onmouseover="this.src='/img/mainMode/unhover/story_mode_light.png'" onmouseout="this.src='/img/mainMode/unhover/story_mode_dark.png'">
+            onmouseover="this.src='/img/mainMode/hover/story_mode_light.png'" onmouseout="this.src='/img/mainMode/unhover/story_mode_dark.png'">
         </a>
 
         <a class="img-mode" href="/inTime">
             <img class="img-fluid" src="/img/mainMode/unhover/time_challenge_mode_dark.png" alt="Time Challenge Mode"
-            onmouseover="this.src='/img/mainMode/unhover/time_challenge_mode_light.png'" onmouseout="this.src='/img/mainMode/unhover/time_challenge_mode_dark.png'">
+            onmouseover="this.src='/img/mainMode/hover/time_challenge_mode_light.png'" onmouseout="this.src='/img/mainMode/unhover/time_challenge_mode_dark.png'">
         </a>
     </div>
 
