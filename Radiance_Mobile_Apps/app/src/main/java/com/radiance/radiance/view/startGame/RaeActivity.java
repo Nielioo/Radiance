9ab281989c07b9eb57afab4a0b2d413f82cb8b06
annaxatma <83283641+annaxatma@users.noreply.github.com>
2022-01-04 00:10:53 +0900

    back button

--- Radiance_Mobile_Apps/app/src/main/java/com/radiance/radiance/view/startGame/RaeActivity.java
+++ Radiance_Mobile_Apps/app/src/main/java/com/radiance/radiance/view/startGame/RaeActivity.java
@@ -1,6 +1,7 @@
 package com.radiance.radiance.view.startGame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,18 +9,20 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.radiance.radiance.R;
 import com.radiance.radiance.model.Dialogues;
-
-import java.util.ArrayList;
 
 public class RaeActivity extends AppCompatActivity {
 
     private ImageView dialogBox_imageView;
     private TextView dialog_textView;
     private Dialogues model;
+    private ConstraintLayout nextButton_constraintLayout;
+    private LinearLayout option_linearLayout;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,31 +33,37 @@ public class RaeActivity extends AppCompatActivity {
         
         initView();
         setListener();
-
-        model = new Dialogues();
-        dialog_textView.setText(model.getStage1Level1().get(0));
-        int i;
-//        for(i=0; i< ArrayList.s)
     }
 
     private void setListener() {
-        dialogBox_imageView.setOnClickListener(new View.OnClickListener() {
+        model = new Dialogues();
+        dialog_textView.setText(model.getStage1Level1().get(i));
+
+        nextButton_constraintLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Bundle bundle = getIntent().getExtras();
-                String mDrawableName = bundle.getString("bgImage");
-                Bundle setBackground = new Bundle();
-                setBackground.putString("bgImage", String.valueOf(mDrawableName));
-                Intent play = new Intent(RaeActivity.this, PlayActivity.class);
-                play.putExtras(setBackground);
-                startActivity(play);
+                i++;
+                if(i>=model.getStage1Level1().size()){
+                            Bundle bundle = getIntent().getExtras();
+                            String mDrawableName = bundle.getString("bgImage");
+                            Bundle setBackground = new Bundle();
+                            setBackground.putString("bgImage", String.valueOf(mDrawableName));
+                            Intent play = new Intent(RaeActivity.this, PlayActivity.class);
+                            play.putExtras(setBackground);
+                            startActivity(play);
+                }else{
+                    dialog_textView.setText(model.getStage1Level1().get(i));
+                }
             }
-        });
+            });
     }
 
     private void initView() {
-
         dialogBox_imageView = findViewById(R.id.rae_dialogBox_imageView);
         dialog_textView = findViewById(R.id.rae_dialog_textView);
+        nextButton_constraintLayout = findViewById(R.id.rae_nextButton_constraintLayout);
+        option_linearLayout = findViewById(R.id.rae_option_linearLayout);
+
+        option_linearLayout.setVisibility(View.GONE);
     }
 }
