a5f7eaa5a31b598c2a67f8082180335330b87784
annaxatma <83283641+annaxatma@users.noreply.github.com>
2022-01-09 18:08:53 +0900

    dialogues finally done

--- Radiance_Mobile_Apps/app/src/main/java/com/radiance/radiance/model/Dialogues.java
+++ Radiance_Mobile_Apps/app/src/main/java/com/radiance/radiance/model/Dialogues.java
@@ -3,26 +3,435 @@ package com.radiance.radiance.model;
 import java.util.ArrayList;
 
 public class Dialogues {
-    ArrayList<String> stage1Level1 = new ArrayList<>();
-    ArrayList<String> stage1Level5 = new ArrayList<>();
+        ArrayList<String> stage1Level1 = new ArrayList<>();
+        ArrayList<String> stage1Level5 = new ArrayList<>();
+        ArrayList<String> stage1Level10 = new ArrayList<>();
 
-    public Dialogues() {
-        //STAGE 1 LEVEL 1
+        ArrayList<String> stage2Level1 = new ArrayList<>();
+        ArrayList<String> stage2Level4 = new ArrayList<>();
+        ArrayList<String> stage2Level7 = new ArrayList<>();
+        ArrayList<String> stage2Level10 = new ArrayList<>();
+
+        ArrayList<String> stage3Level1 = new ArrayList<>();
+        ArrayList<String> stage3Level3 = new ArrayList<>();
+        ArrayList<String> stage3Level6 = new ArrayList<>();
+        ArrayList<String> stage3Level10 = new ArrayList<>();
+
+        ArrayList<String> stage4Level1 = new ArrayList<>();
+        ArrayList<String> stage4Level3 = new ArrayList<>();
+        ArrayList<String> stage4Level6 = new ArrayList<>();
+        ArrayList<String> stage4Level10 = new ArrayList<>();
+
+        ArrayList<String> stage5Level1 = new ArrayList<>();
+        ArrayList<String> stage5Level5 = new ArrayList<>();
+        ArrayList<String> stage5Level10 = new ArrayList<>();
+
+        ArrayList<String> stage6Level1 = new ArrayList<>();
+        ArrayList<String> stage6Level4 = new ArrayList<>();
+        ArrayList<String> stage6Level6 = new ArrayList<>();
+        ArrayList<String> stage6Level10 = new ArrayList<>();
+
+        ArrayList<String> stage7Level1 = new ArrayList<>();
+        ArrayList<String> stage7Level5 = new ArrayList<>();
+        ArrayList<String> stage7Level10 = new ArrayList<>();
+
+        public Dialogues() {
+                //STAGE 1 LEVEL 1
         stage1Level1.add("Pemerintah telah memberikan izin untuk membangun pulau buatan tidak jauh dari kota. Untuk memudahkan akses ke pulau itu, gubernur memerintahkan untuk membangun jembatan gantung.");
         stage1Level1.add("Wihiii~");
         stage1Level1.add("Rae tidak sabar melihat tempatnya.");
         stage1Level1.add("Oh iya, tiap level yang kamu selesaikan akan membuat progress pembuatan jembatan bertambah. Semoga berhasil!");
-        stage1Level1.add("Rae akan memberimu semangat! Go go yeay!");
+        stage1Level1.add("Rae akan memberimu semangat! Go go!");
 
-        //STAGE 1 LEVEL 5
+                //STAGE 1 LEVEL 5
         stage1Level5.add("Sepertinya telah terjadi kesalahan teknis dalam proses pembangunan, oleh karena itu, kecepatanmu dalam menyelesaikan perkerjaan ini akan semakin lambat.");
         stage1Level5.add("Tidak ada hal yang bisa menghambatku");
         stage1Level5.add("Semua hambatan bisa kulalui bersamamu");
         stage1Level5.add("Yup, Rae yakin dengan kemampuanmu");
         stage1Level5.add("Tentu saja, Rae akan selalu mendukungmu!");
 
+                //STAGE 1 LEVEL 10
+        stage1Level10.add("Yeay!!!");
+        stage1Level10.add("Kamu berhasil menyelesaikan jembatan gantung!!");
+        stage1Level10.add("Rae tahu kamu pasti bisa hehehe~");
+
+                //STAGE 2 LEVEL 1
+        stage2Level1.add("Warga kota sangat membutuhkan bantuan. Coba bantu beberapa dari mereka, dan mereka mungkin akan memberi sebuah kejutan.");
+        stage2Level1.add("Akhir-akhir ini sepertinya ada lebih banyak orang yang membutuhkan bantuan di kota ini.");
+        stage2Level1.add("Menurut list yang Rae dapatkan, ada seorang pria yang sedang memperbaiki pipa air dengan membuka keran air yang macet.");
+        stage2Level1.add("Ayo kita bantu pria itu!");
+
+                //STAGE 2 LEVEL 4
+        stage2Level4.add("Kerja bagus! Kamu berhasil membantu pria itu memperbaiki keran airnya.");
+        stage2Level4.add("Memperbaiki keran air adalah hal yang mudah");
+        stage2Level4.add("Kalau Rae lihat listnya lagi, orang selanjutnya yang membutuhkan bantuan adalah seorang anak ballerina. Dia membutuhkan bantuan dalam latihannya agar dia dapat menari dengan indah.");
+        stage2Level4.add("Menari ballet terdengar menyenangkan");
+        stage2Level4.add("Mungkin kamu juga bisa belajar menari ballet langsung darinya. Ayo kita ke sana!");
+
+                //STAGE 2 LEVEL 7
+        stage2Level7.add("Kamu baru saja membantu seorang anak perempuan mewujudkan impiannya menjadi seorang ballerina.");
+        stage2Level7.add("Sudah 2 orang yang kita bantu yang artinya tersisa satu orang terakhir.");
+        stage2Level7.add("Hm?");
+        stage2Level7.add("Sepertinya orang ini tidak asing bagi kita.");
+        stage2Level7.add("Hmm... Dari mukanya, kalian tidak terlihat seumuran...");
+        stage2Level7.add("OH! Dia terlihat mirip dengan tetangga rumahmu yang sering kita jumpai di pagi hari.");
+        stage2Level7.add("Dia sepertinya mengalami kesulitan memutar ganggang pintu rumahnya sambil membawa banyak belanjaan.");
+        stage2Level7.add("Tidak bisa diabaikan ketika orang terdekat membutuhkan bantuan");
+        stage2Level7.add("Baiklah, mari kita bantu tetanggamu dan selesaikan hari ini.");
+
+                //STAGE 2 LEVEL 10
+        stage2Level10.add("Woohoo!");
+        stage2Level10.add("Kita telah berhasil membantu seluruh orang yang membutuhkan bantuan hari ini.");
+        stage2Level10.add("Hari ini merupakan hari yang panjang, kamu bisa beristirahat jika kamu mau.");
+        stage2Level10.add("Dan ingat, jika ada orang yang membutuhkan bantuanmu, jangan ragu untuk membantunya.");
+        stage2Level10.add("Sampai jumpa nanti!");
+
+                //STAGE 3 LEVEL 1
+        stage3Level1.add("Kamu pergi berkemah dengan teman-temanmu di hutan, tetapi akhir dari perjalanan tiga hari ini ditunda karena banjir.");
+        stage3Level1.add("Akan butuh setidaknya satu setengah hari untuk airnya menyusut, tetapi persediaan makanan yang kalian bawa tidak akan cukup sampai banjir mereda.");
+        stage3Level1.add("Kalian harus mencari persediaan makanan.");
+
+                //STAGE 3 LEVEL 3
+        stage3Level3.add("Kamu berhasil menemukan bebuahan di hutan, tetapi itu saja tidak cukup.");
+        stage3Level3.add("Kalian membutuhkan daging. Busur dan panah dapat membantu.");
+        stage3Level3.add("Buatlah busur dan panah untuk berburu!");
+
+                //STAGE 3 LEVEL 6
+        stage3Level6.add("Gunakan senjata yang telah berhasil dibuat untuk berburu hewan.");
+        stage3Level6.add("Hati-hati dengan binatang buas yang mungkin saja ada di hutan. Mulai sekarang, akan bertambah sulit.");
+
+                //STAGE 3 LEVEL 10
+        stage3Level10.add("Pheww....");
+        stage3Level10.add("Kamu berhasil mencari persediaan makanan dan bertahan hidup selagi di hutan.");
+        stage3Level10.add("Jika ingin berkemah lain kali, jangan lupa membawa survival kit. Akan sangat membantumu untuk bertahan hidup di tempat yang tidak dikenal. Dan selalu hati-hati!");
+
+
+            //STAGE 4 LEVEL 1
+            stage4Level1.add("Kamu berencana keliling kota untuk lebih mengenalnya, tetapi di tengah jalan, banmu kempes karena terkena paku dan mengharuskanmu untuk menggantinya. perbaiki ban agar bisa melanjutkan perjalanan.");
+            stage4Level1.add("Hmmm............");
+            stage4Level1.add("Kita tidak bisa melanjutkan perjalanan dengan ban kempes.......");
+            stage4Level1.add("Kita harus memperbaikinya.");
+            stage4Level1.add("Tentu saja. Pertama kita harus mencari alat untuk mengangkat mobil");
+
+            //STAGE 4 LEVEL 3
+            stage4Level3.add("Hmm...");
+            stage4Level3.add("Apa kita mempunyai ban tambahan?");
+            stage4Level3.add("...");
+            stage4Level3.add("Sepertinya tidak... Kita harus mencari cara lain");
+
+            //STAGE 4 LEVEL 6
+            stage4Level6.add("OH! Rae tahu! Rae tahu!");
+            stage4Level6.add("Kita membutuhkan pompa angin, lubangnya akan kita tutup menggunakan sesuatu untuk sementara sampai kita bisa membawanya ke bengkel");
+            stage4Level6.add("Rae pintar kan muahaha~");
+
+            //STAGE 4 LEVEL 10
+            stage4Level10.add("Fiuh!");
+            stage4Level10.add("Kita berhasil!");
+            stage4Level10.add("Memang rae mempunyai ide yang bagus!");
+            stage4Level10.add("Jalan-jalannya nanti saja, ayo ke bengkel.");
+
+            //STAGE 5 LEVEL 1
+            stage6Level1.add("Kabar buruk!");
+            stage6Level1.add("Kita sepertinya diculik!");
+            stage6Level1.add("Hueeeeeeeeeeeeeeeeee");
+            stage6Level1.add("Apa yang harus kita lakukan?!");
+            stage6Level1.add("Rae pun tidak tahu dimana ini");
+            stage6Level1.add("Kita harus mencari jalan keluar dengan cepat!");
+            stage5Level1.add("Aaaa!");
+            stage5Level1.add("Rae mendengar jejak kaki!");
+            stage5Level1.add("Sepertinya itu mereka!!!");
+            stage5Level1.add("Cepat! Sembunyi dan coba untuk kalahkan mereka! Rae yakin kamu pasti bisa!");
+
+            //STAGE 5 LEVEL 5
+            stage5Level5.add("Sekarang dimana kita?");
+            stage5Level5.add("Hutan? Hutan dimana ini?!");
+            stage5Level5.add("Dilihat dari kegelapan hutan, sepertinya ini lewat dari tengah malam...");
+            stage5Level5.add("Disini pun tidak ada sinyal...");
+            stage5Level5.add("Dilihat dari kegelapan hutan, sepertinya ini lewat dari tengah malam...");
+            stage5Level5.add("Arghh!!!!!");
+            stage5Level5.add("Pertama! kita harus membuat api untuk bertahan hidup malam ini!");
+            stage5Level5.add("Buat api sebelum rae mati kedinginan!!!");
+            stage5Level5.add("Apa? Rae walaupun robot, tetapi rae mempunyai sensor untuk suhu badan, hmph!");
+
+            //STAGE 5 LEVEL 10
+            stage5Level10.add("Hehehe~");
+            stage5Level10.add("Kita berhasil kabur dan selamat~");
+            stage5Level10.add("Rae pikir rae akan mati disana.");
+            stage5Level10.add("Lain kali, kita harus hati-hati, jangan sampai diculik!");
+            stage5Level10.add("Mereka pikir rae gampang dicuri apa!");
+            stage5Level10.add("Tapi mereka mempunyai selera jika menculik rae~");
+
+            //STAGE 6 LEVEL 1
+            stage6Level1.add("Kamu memutuskan untuk pergi ke pantai di akhir pekan. Rae yang menemanimu sepertinya tidak sabar untuk bermain air");
+            stage6Level1.add("Wihiiiiiiiiiiii~");
+            stage6Level1.add("Rae tidak sabar untuk masuk ke air.");
+            stage6Level1.add("Tenang saja, rae anti air. Dan rae sangat suka dengan air!");
+            stage6Level1.add("Hm??");
+            stage6Level1.add("Apa kamu mendengar itu?");
+            stage6Level1.add("Rae mendengar suara anak kecil.");
+            stage6Level1.add("Kita harus mencari tahu!");
+            stage6Level1.add("Ikuti sumber suara itu!");
+
+            //STAGE 6 LEVEL 4
+            stage6Level4.add("Hm... Tidak ada siapa-siapa disini...");
+            stage6Level4.add("Apa rae salah mendengar? Tidak mungkin....");
+            stage6Level4.add("Hm? Apa ini?? Secarik kertas?");
+            stage6Level4.add("'Lihat kedalam laut dan kamu mungkin saja menemukan hal tak terduga', katanya... Dan ada beberapa petunjuk yang tidak dimengerti rae. Siapapun yang membuat ini myenyesalkan sekali orangnya!");
+            stage6Level4.add("...");
+            stage6Level4.add("Kita harus pergi!");
+            stage6Level4.add("Ini bukan karena rae penasaran dengan harta karun yang tersembunyi. Teatpi untuk memastikan tidak ada orang lain yang ketipu!");
+            stage6Level4.add("Kita memerlukan kapal.");
+            stage6Level4.add("Oh, lihat! ada kapal yang akan mengingglkan pantai! Ayo kita ikuti!");
+
+            //STAGE 6 LEVEL 6
+            stage6Level6.add("...");
+            stage6Level6.add("Sekarang apa??");
+            stage6Level6.add("Pecahkan petunjuknya?");
+            stage6Level6.add("Rae akan mengandalkanmu untuk memecahkan petunjuk ini.");
+
+            //STAGE 6 LEVEL 10
+            stage6Level10.add("OH! OH!");
+            stage6Level10.add("Lihat!!!");
+            stage6Level10.add("Kita menemukan peti!");
+            stage6Level10.add("Buka petinya! Rae pingin lihat!");
+            stage6Level10.add("...");
+            stage6Level10.add("APA INI!!!! PETINYA KOSONG!!!!!!!");
+            stage6Level10.add("Pembohong!!! Kita ditipu!!!");
+
+            //STAGE 7 LEVEL 1
+            stage7Level1.add("Kamu ingin membeli kacamata karena penglihatanmu menurun akibat terlalu sering bermain di depan layar komputer.");
+            stage7Level1.add("Ayo kita ke mall.");
+            stage7Level1.add("Kamu bisa membeli segalanya disana!");
+
+            //STAGE 7 LEVEL 5
+            stage7Level5.add("Ketika kamu dan rae hendak masuk melalui pintu utama, kalian mendengar suara keras!");
+            stage7Level5.add("Apa itu???");
+            stage7Level5.add("Menurut rae itu sepertinya suara mobil kecelakaan");
+            stage7Level5.add("Ayo kita lihat sebentar sebelum mencari kacamatamu.");
+            stage7Level5.add("Tapi hati-hati, banyak orang juga menuju kesana. Jangan sampai kita terpisah.");
+
+            //STAGE 7 LEVEL 10
+            stage7Level10.add("Fiuhhh.");
+            stage7Level10.add("Hari ini cukup berat.");
+            stage7Level10.add("Kupikir hari ini akan menyenagkan karena kita pergi ke mall....");
+            stage7Level10.add("Sudahlah, yang penting kamu mendapat kacamatamu.");
+            stage7Level10.add("Kita akhiri hari ini ya. Rae butuh direcharge.");
+
         this.stage1Level1 = stage1Level1;
         this.stage1Level5 = stage1Level5;
+        this.stage1Level10 = stage1Level10;
+
+        this.stage2Level1 = stage2Level1;
+        this.stage2Level4 = stage2Level4;
+        this.stage2Level7 = stage2Level7;
+        this.stage2Level10 = stage2Level10;
+
+        this.stage3Level1 = stage3Level1;
+        this.stage3Level3 = stage3Level3;
+        this.stage3Level6 = stage3Level6;
+        this.stage3Level10 = stage3Level10;
+
+        this.stage4Level1 = stage4Level1;
+        this.stage4Level3 = stage4Level3;
+        this.stage4Level6 = stage4Level6;
+        this.stage4Level10 = stage4Level10;
+
+        this.stage5Level1 = stage5Level1;
+        this.stage5Level5 = stage5Level5;
+        this.stage5Level10 = stage5Level10;
+
+        this.stage6Level1 = stage6Level1;
+        this.stage6Level4 = stage6Level4;
+        this.stage6Level6 = stage6Level6;
+        this.stage6Level10 = stage6Level10;
+
+        this.stage7Level1 = stage7Level1;
+        this.stage7Level5 = stage7Level5;
+        this.stage7Level10 = stage7Level10;
+    }
+
+    public ArrayList<String> getStage5Level1() {
+        return stage5Level1;
+    }
+
+    public void setStage5Level1(ArrayList<String> stage5Level1) {
+        this.stage5Level1 = stage5Level1;
+    }
+
+    public ArrayList<String> getStage5Level5() {
+        return stage5Level5;
+    }
+
+    public void setStage5Level5(ArrayList<String> stage5Level5) {
+        this.stage5Level5 = stage5Level5;
+    }
+
+    public ArrayList<String> getStage5Level10() {
+        return stage5Level10;
+    }
+
+    public void setStage5Level10(ArrayList<String> stage5Level10) {
+        this.stage5Level10 = stage5Level10;
+    }
+
+    public ArrayList<String> getStage6Level1() {
+        return stage6Level1;
+    }
+
+    public void setStage6Level1(ArrayList<String> stage6Level1) {
+        this.stage6Level1 = stage6Level1;
+    }
+
+    public ArrayList<String> getStage6Level4() {
+        return stage6Level4;
+    }
+
+    public void setStage6Level4(ArrayList<String> stage6Level4) {
+        this.stage6Level4 = stage6Level4;
+    }
+
+    public ArrayList<String> getStage6Level6() {
+        return stage6Level6;
+    }
+
+    public void setStage6Level6(ArrayList<String> stage6Level6) {
+        this.stage6Level6 = stage6Level6;
+    }
+
+    public ArrayList<String> getStage6Level10() {
+        return stage6Level10;
+    }
+
+    public void setStage6Level10(ArrayList<String> stage6Level10) {
+        this.stage6Level10 = stage6Level10;
+    }
+
+    public ArrayList<String> getStage7Level1() {
+        return stage7Level1;
+    }
+
+    public void setStage7Level1(ArrayList<String> stage7Level1) {
+        this.stage7Level1 = stage7Level1;
+    }
+
+    public ArrayList<String> getStage7Level5() {
+        return stage7Level5;
+    }
+
+    public void setStage7Level5(ArrayList<String> stage7Level5) {
+        this.stage7Level5 = stage7Level5;
+    }
+
+    public ArrayList<String> getStage7Level10() {
+        return stage7Level10;
+    }
+
+    public void setStage7Level10(ArrayList<String> stage7Level10) {
+        this.stage7Level10 = stage7Level10;
+    }
+
+    public ArrayList<String> getStage1Level10() {
+        return stage1Level10;
+    }
+
+    public void setStage1Level10(ArrayList<String> stage1Level10) {
+        this.stage1Level10 = stage1Level10;
+    }
+
+    public ArrayList<String> getStage2Level1() {
+        return stage2Level1;
+    }
+
+    public void setStage2Level1(ArrayList<String> stage2Level1) {
+        this.stage2Level1 = stage2Level1;
+    }
+
+    public ArrayList<String> getStage2Level4() {
+        return stage2Level4;
+    }
+
+    public void setStage2Level4(ArrayList<String> stage2Level4) {
+        this.stage2Level4 = stage2Level4;
+    }
+
+    public ArrayList<String> getStage2Level7() {
+        return stage2Level7;
+    }
+
+    public void setStage2Level7(ArrayList<String> stage2Level7) {
+        this.stage2Level7 = stage2Level7;
+    }
+
+    public ArrayList<String> getStage2Level10() {
+        return stage2Level10;
+    }
+
+    public void setStage2Level10(ArrayList<String> stage2Level10) {
+        this.stage2Level10 = stage2Level10;
+    }
+
+    public ArrayList<String> getStage3Level1() {
+        return stage3Level1;
+    }
+
+    public void setStage3Level1(ArrayList<String> stage3Level1) {
+        this.stage3Level1 = stage3Level1;
+    }
+
+    public ArrayList<String> getStage3Level3() {
+        return stage3Level3;
+    }
+
+    public void setStage3Level3(ArrayList<String> stage3Level3) {
+        this.stage3Level3 = stage3Level3;
+    }
+
+    public ArrayList<String> getStage3Level6() {
+        return stage3Level6;
+    }
+
+    public void setStage3Level6(ArrayList<String> stage3Level6) {
+        this.stage3Level6 = stage3Level6;
+    }
+
+    public ArrayList<String> getStage3Level10() {
+        return stage3Level10;
+    }
+
+    public void setStage3Level10(ArrayList<String> stage3Level10) {
+        this.stage3Level10 = stage3Level10;
+    }
+
+    public ArrayList<String> getStage4Level1() {
+        return stage4Level1;
+    }
+
+    public void setStage4Level1(ArrayList<String> stage4Level1) {
+        this.stage4Level1 = stage4Level1;
+    }
+
+    public ArrayList<String> getStage4Level3() {
+        return stage4Level3;
+    }
+
+    public void setStage4Level3(ArrayList<String> stage4Level3) {
+        this.stage4Level3 = stage4Level3;
+    }
+
+    public ArrayList<String> getStage4Level6() {
+        return stage4Level6;
+    }
+
+    public void setStage4Level6(ArrayList<String> stage4Level6) {
+        this.stage4Level6 = stage4Level6;
+    }
+
+    public ArrayList<String> getStage4Level10() {
+        return stage4Level10;
+    }
+
+    public void setStage4Level10(ArrayList<String> stage4Level10) {
+        this.stage4Level10 = stage4Level10;
     }
 
     public ArrayList<String> getStage1Level5() {
